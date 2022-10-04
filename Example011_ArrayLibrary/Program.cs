94b2625b0f33e60d7734a77045abb3f416926e12
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-04 18:47:52 +0200

    add example 7,8,9,10,11

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+// FillArray - наименование метода
+// в качестве аргумента будет приниматься коллекция, collection - название аргумента
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // в массиве по умолчанию будет 10 элементов
+// new int[10] - создай новый массив в котором будет 10 элементов, по умолчанию заполнится нулями
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
